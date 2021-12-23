3f302497ae3fab259fa78c087d63711b9d57451c
MusaTG <49027797+MusaTG@users.noreply.github.com>
2021-12-23 12:11:41 +0300

    proje oluşturuldu

---
+++ Core/Entities/Concrete/County.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities.Concrete
+{
+    public class County:IEntity
+    {
+        public int CountyId { get; set; }
+        public string CountyName { get; set; }
+    }
+}
